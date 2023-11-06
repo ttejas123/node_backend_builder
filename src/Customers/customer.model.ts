46061ab81c97836662952ee9114dbc75d767a1e4
Tejas <tthakare73@gmail.com>
2023-11-06 07:57:44 +0530

    Created Script to genarate backednd design v1

--- src/Customers/customer.model.ts
+++
@@ -1,9 +0,0 @@
-
-  export interface customer {
-  // Define your model schema here
-    customer_id?: number,
-  first_name?: string,
-  last_name?: string,
-  age?: number,
-  country?: string,
-}

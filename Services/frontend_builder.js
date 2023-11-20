1197abbca879fc809e5bbc0cb4821fe24e7a07bd
Tejas <tthakare73@gmail.com>
2023-11-20 08:49:41 +0530

    Added Form Style

--- Services/frontend_builder.js
+++ Services/frontend_builder.js
@@ -8,14 +8,30 @@ const generateReactComponent = (tableName, fields, ComponentName) => {
     const fieldName = field.name;
     const fieldType = field.modelType;
 
+    if(fieldName === 'id') return '';
+
     return (
       `<div key="${fieldName}">
-        <label>${fieldName}:</label>
-        <input
-          type="${fieldType}"
-          name="${fieldName}"
-          onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}
-        />
+        <label className="input">${fieldName}:</label>
+        ${
+          fieldType === 'boolean' ? (
+            `
+            <input 
+            className='input__field'
+            type="checkbox" 
+            name="${fieldName}"
+            onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.checked })}
+            />
+            `
+          ) : (
+            `<input
+            className='input__field'
+            type="${fieldType}"
+            name="${fieldName}"
+            onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}
+          />`
+          )
+        }
       </div>`
     );
   }).join('\n');
@@ -24,6 +40,7 @@ const generateReactComponent = (tableName, fields, ComponentName) => {
 import { useState } from 'react';
 import axios from 'axios';
 import Modal from 'react-modal'; // Import the modal library
+import '../../assets/FormStyles.scss'
 
 const ${ComponentName}Form = ({ isOpen, onRequestClose }) => {
   const [formData, setFormData] = useState({});
@@ -43,8 +60,9 @@ const ${ComponentName}Form = ({ isOpen, onRequestClose }) => {
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       contentLabel="${ComponentName} Form"
+      style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}
     >
-      <div>
+      <div className='card card--accent'>
         <h2>${ComponentName} Form</h2>
         <form onSubmit={handleSubmit}>
           ${fieldElements}
@@ -114,7 +132,6 @@ export default ${ComponentName}Form;
   const generateReactMainApp = (tableData) => {
     const imports = tableData.map(({ table_name }) => {
         return (`
-// import ${table_name}Form from './component/${table_name}/${table_name}Form';
 import ${table_name}DataTable from './component/${table_name}/${table_name}DataTable';
         `)
     }).join('\n');
@@ -199,7 +216,8 @@ export default defineConfig({
               "eslint-plugin-react-hooks": "^4.6.0",
               "eslint-plugin-react-refresh": "^0.4.4",
               "vite": "^5.0.0",
-              "react-modal": "^3.16.1"
+              "react-modal": "^3.16.1",
+              "sass": "^1.69.5"
             }
           }
           
@@ -281,6 +299,149 @@ dist-ssr
 
     `)
   }
+
+  const generateFormScss = () => {
+    return (
+      `
+      @import url('https://rsms.me/inter/inter.css');
+
+:root {
+  --color-light: white;
+  --color-dark: #212121;
+  --color-signal: #fab700;
+  
+  --color-background: var(--color-light);
+  --color-text: var(--color-dark);
+  --color-accent: var(--color-signal);
+  
+  --size-bezel: .5rem;
+  --size-radius: 4px;
+  
+  line-height: 1.4;
+  
+  font-family: 'Inter', sans-serif;
+  font-size: calc(.6rem + .4vw);
+  color: var(--color-text);
+  background: var(--color-background);
+  font-weight: 300;
+  padding: 0 calc(var(--size-bezel) * 3);
+}
+
+h1, h2, h3 {
+  font-weight: 900;
+}
+
+mark {
+  background: var(--color-accent);
+  color: var(--color-text);
+  font-weight: bold;
+  padding: 0 0.2em;
+}
+
+.card {
+  background: var(--color-background);
+  padding: calc(4 * var(--size-bezel));
+  margin-top: calc(4 * var(--size-bezel));
+  border-radius: var(--size-radius);
+  border: 3px solid var(--color-shadow, currentColor);
+  box-shadow: .5rem .5rem 0 var(--color-shadow, currentColor);
+  
+  &--inverted {
+    --color-background: var(--color-dark);
+    color: var(--color-light);
+    --color-shadow: var(--color-accent);
+  }
+  
+  &--accent {
+    --color-background: var(--color-signal);
+    --color-accent: var(--color-light);
+    color: var(--color-dark);
+  }
+  
+  *:first-child {
+    margin-top: 0;
+  }
+}
+
+
+.l-design-widht {
+  max-width: 40rem;
+  padding: 1rem;
+}
+
+.input {
+  position: relative;
+  
+  &__label {
+    position: absolute;
+    left: 0;
+    top: 0;
+    padding: calc(var(--size-bezel) * 0.75) calc(var(--size-bezel) * .5);
+    margin: calc(var(--size-bezel) * 0.75 + 3px) calc(var(--size-bezel) * .5);
+    background: pink;
+    white-space: nowrap;
+    transform: translate(0, 0);
+    transform-origin: 0 0;
+    background: var(--color-background);
+    transition: transform 120ms ease-in;
+    font-weight: bold;
+    line-height: 1.2;
+  }
+  &__field {
+    box-sizing: border-box;
+    display: block;
+    width: 100%;
+    border: 3px solid currentColor;
+    padding: calc(var(--size-bezel) * 1.5) var(--size-bezel);
+    color: currentColor;
+    background: transparent;
+    border-radius: var(--size-radius);
+    
+    &:focus,
+    &:not(:placeholder-shown) {
+      & + .input__label {
+        transform: translate(.25rem, -65%) scale(.8);
+        color: var(--color-accent);
+      }
+    }
+  }
+}
+
+
+.button-group {
+  margin-top: calc(var(--size-bezel) * 2.5);
+}
+
+button {
+  color: currentColor;
+  padding: var(--size-bezel) calc(var(--size-bezel) * 2);
+  background: var(--color-accent);
+  border: none;
+  border-radius: var(--size-radius);
+  font-weight: 900;
+  
+  &[type=reset]{
+    background: var(--color-background);
+    font-weight: 200;
+  } 
+}
+
+button + button {
+  margin-left: calc(var(--size-bezel) * 2);
+}
+
+.icon {
+  display: inline-block;
+  width: 1em; height: 1em;
+  margin-right: .5em;
+}
+
+.hidden {
+  display: none;
+}
+      `
+    )
+  }
   
 const init = (tableData) => {
     const folderPathReact = path.join(__dirname, base_forlder);
@@ -291,6 +452,7 @@ const init = (tableData) => {
     const srcfolderPathReact = path.join(folderPathReact, 'src')
     const publicfolderPathReact = path.join(folderPathReact, 'public')
     const componentfolderPathReact = path.join(srcfolderPathReact, 'component')
+    const assetsfolderPathReact = path.join(srcfolderPathReact, 'assets')
 
     if (!fs.existsSync(srcfolderPathReact)) {
         fs.mkdirSync(srcfolderPathReact);
@@ -302,6 +464,10 @@ const init = (tableData) => {
 
     if (!fs.existsSync(componentfolderPathReact)) {
         fs.mkdirSync(componentfolderPathReact);
+    }
+    
+    if (!fs.existsSync(assetsfolderPathReact)) {
+        fs.mkdirSync(assetsfolderPathReact);
     }
 
     tableData.forEach(({ name, fields, table_name }) => {
@@ -315,6 +481,8 @@ const init = (tableData) => {
 
     fs.writeFileSync(path.join(srcfolderPathReact, 'App.jsx'), generateReactMainApp(tableData));
     fs.writeFileSync(path.join(srcfolderPathReact, 'main.jsx'), generateReactEntryFile());
+
+    fs.writeFileSync(path.join(assetsfolderPathReact, 'FormStyles.scss'), generateFormScss());
 
     fs.writeFileSync(path.join(folderPathReact, 'vite.config.js'), generateVitaConfigTemplate());
     fs.writeFileSync(path.join(folderPathReact, 'package.json'), generatePackageJSONTemplate());
